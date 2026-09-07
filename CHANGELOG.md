ad3ed020d38d2f7ed9817e027af5fab72f0b35e6
Gonzalo25U <gonzalo.s.ulloa@gmail.com>
2026-09-06 21:00:51 -0300

    V1.2.0: Cambios en los estilos y el diseño

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,2 @@
 V1.0.0: Version inicial con los archivos base que se utilizaran
 V1.2.0: Cambios en los estilos y el diseño
-V2.0.0: Añadiendo validacion jwt
--- src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-export function decodeJwt(token) {
-  try {
-    const payloadBase64 = token.split('.')[1];
-    const payloadJson = atob(payloadBase64.replace(/-/g, '+').replace(/_/g, '/'));
-    return JSON.parse(payloadJson);
-  } catch {
-    return null;
-  }
-}
